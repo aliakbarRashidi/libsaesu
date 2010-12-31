5288048e1a3262010074f5ff034ade22fdb86943
Robin Burchell <robin.burchell@collabora.co.uk>
2010-12-31 02:50:09 +0000

    Add microseconds to debug output.

--- include/sglobal.h
+++ include/sglobal.h
@@ -19,11 +19,12 @@
 #define SGLOBAL_H
 
 #include <QtCore/QDebug>
+#include <QtCore/QDateTime>
 
 #define S_VERIFY(x, y) (!(x) && ((qWarning() << Q_FUNC_INFO << y), Q_ASSERT(x), true))
 #define S_VERIFY_OR_DIE(x, y) if (!(x)) { qFatal("%s%s", Q_FUNC_INFO, y); }
 
-#define sDebug() qDebug() << "[debug]" << Q_FUNC_INFO << "-"
-#define sWarning() qWarning() << "[warning]" << Q_FUNC_INFO << "-"
+#define sDebug() qDebug() << "[debug]" << QDateTime::currentMSecsSinceEpoch() << Q_FUNC_INFO << "-"
+#define sWarning() qWarning() << "[warning]" << QDateTime::currentMSecsSinceEpoch() << Q_FUNC_INFO << "-"
 
 #endif // SGLOBAL_H
